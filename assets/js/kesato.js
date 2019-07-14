b05b5f494b76dd7af8f71162a73db9ded5348995
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-14 13:31:03 +0800

    first commit

---
+++ assets/js/kesato.js
@@ -0,0 +1,9 @@
+(function ($) {
+    "use strict";
+
+    // PRELOADER
+    $(window).on('load', function(){
+        const preloader = $('.preloader');
+        preloader.fadeOut(1000);
+    });
+}(jQuery))
