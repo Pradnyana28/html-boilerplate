e314c2f3640008aea7ba7ac9577288e156d13583
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-17 16:46:22 +0800

    create shortcode to display specific post

--- wp-content/themes/understrap/inc/setup.php
+++ wp-content/themes/understrap/inc/setup.php
@@ -139,3 +139,22 @@ if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 		return $post_excerpt;
 	}
 }
+
+if ( ! function_exists( 'display_post_shortcode' ) ) {
+	/**
+	 * Adds display post shortcode
+	 */
+	function display_post_shortcode( $attr ) {
+		ob_start();
+		$attributes = shortcode_atts( [
+			'display' => 3,
+			'order' => 'descending'
+		], $attr );
+
+		$posts = get_posts( [ 'numberposts' => $attributes['display'], 'order' => $attributes['order'] ] );
+		require_once get_theme_file_path() .'/loop-templates/post.php';
+		return ob_get_clean();
+	}
+
+	add_shortcode( 'kesato-post', 'display_post_shortcode' );
+}
