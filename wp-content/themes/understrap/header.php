3d37bf1b97124f00cf48263c7f16f7b6d0f72d50
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-17 14:06:06 +0800

    convert menu to wordpress structure

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme.
  *
- * Displays all of the <head> section and everything up till <div id="content">
+ * Displays all of the <header> section and everything up till <div id="content">
  *
  * @package understrap
  */
@@ -12,6 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $container = get_theme_mod( 'understrap_container_type' );
+$extra_button = get_field( 'extra_button' );
+$navigation_logo = get_field( 'logo' );
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -29,58 +31,64 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div>
 
 <?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+<header class="kst-header" id="page">
+	<?php if ( $navigation_logo ): ?>
+		<div class="logo-wrapper">
+			<img src="<?= $navigation_logo ?>" alt="" class="logo">
+		</div>
+	<?php endif; ?>
+
+	<div class="navbar-top d-flex">
+		<div class="container">
+			<ul>
+				<li><a href="/">Menu 1</a></li>
+				<li><a href="/">Menu 2</a></li>
+			</ul>
+		</div>
+	</div>
 
 	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
-
-		<?php if ( 'container' == $container ) : ?>
+		<nav class="main-menu">
+			<?php if ( 'container' == $container ) : ?>
 			<div class="container">
-		<?php endif; ?>
+			<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+				<div class="row d-flex">
+					<div class="col-xl-3 col-lg-4 col-md-12">
+						<button class="menu-button d-block d-lg-none" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+							<i class="icofont-navigation-menu"></i>
+						</button>
+					</div>
+			
+					<div class="col-xl-<?= $extra_button ? '7' : '9' ?> d-flex align-items-center col-lg-8 col-md-12">
+						<!-- The WordPress Menu goes here -->
+						<?php wp_nav_menu(
+							array(
+								'theme_location'  => 'primary',
+								'container_class' => 'main-menu d-none d-lg-block d-md-none d-sm-none',
+								'container_id'    => 'navbarNavDropdown',
+								'menu_class'      => '',
+								'fallback_cb'     => '',
+								'menu_id'         => 'main-menu',
+								'depth'           => 2,
+								'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+							)
+						); ?>
+					</div>
+			
+					<?php if ( $extra_button ): ?>
+						<div class="col-xl-2 col-lg-2 d-flex align-items-center">
+							<a href="<?= $extra_button ?>" class="contact-us-btn btn d-block w-100 btn-default-outline">Contact Us</a>
+						</div>
+					<?php endif; ?>
+				</div>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
 			<?php if ( 'container' == $container ) : ?>
 			</div><!-- .container -->
 			<?php endif; ?>
+		</nav>
 
-		</nav><!-- .site-navigation -->
-
-	</div><!-- #wrapper-navbar end -->
+</header><!-- #wrapper-navbar end -->
--- wp-content/themes/understrap/inc/setup.php
+++ wp-content/themes/understrap/inc/setup.php
@@ -47,6 +47,7 @@ if ( ! function_exists ( 'understrap_setup' ) ) {
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Menu', 'understrap' ),
+			'topbar' => __( 'Top Bar Menu', 'understrap' )
 		) );
 
 		/*
