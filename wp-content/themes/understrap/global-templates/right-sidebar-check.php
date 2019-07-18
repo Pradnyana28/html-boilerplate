c246062dbf1d173ead41f6618cdd10f37c539cd5
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-18 16:33:36 +0800

    hide sidebar for programs page

--- wp-content/themes/understrap/global-templates/right-sidebar-check.php
+++ wp-content/themes/understrap/global-templates/right-sidebar-check.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' ); ?>
 
 <?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
-
-	<?php get_template_part( 'sidebar-templates/sidebar', 'right' ); ?>
-
+	<?php if( $post->post_type != 'programs' ): ?>
+		<?php get_template_part( 'sidebar-templates/sidebar', 'right' ); ?>
+	<?php endif; ?>
 <?php endif; ?>
