d9cec39775a72bc55a4aa8ff876dece932b50ee8
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-18 15:13:23 +0800

    create program page template

---
+++ wp-content/themes/understrap/page-templates/programs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Programs Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php if ( is_front_page() ) : ?>
+  <?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+	<main class="site-main" id="main" role="main">
+
+		
+
+	</main><!-- #main -->
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php get_footer(); ?>
