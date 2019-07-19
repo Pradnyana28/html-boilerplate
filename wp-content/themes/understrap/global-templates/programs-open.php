58363452ebb584270cb6d03aa0eb0df862659e8c
Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-19 14:46:05 +0800

    customize programs content

--- wp-content/themes/understrap/global-templates/programs-open.php
+++ wp-content/themes/understrap/global-templates/programs-open.php
@@ -8,6 +8,10 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$subtitle = get_field( 'subtitle' );
+$tagline  = get_field( 'tagline' );
+$month    = get_field( 'month' );
 ?>
 
 <div class="cestm-wrapper shadow">
@@ -17,3 +21,10 @@ if ( ! defined( 'ABSPATH' ) ) {
     </div>
 
     <div class="p-4">
+        <?php if ( $month ): ?>
+            <span class="btn btn-default-outline mb-4 no-br pl-5 pr-5"><?= $month ?></span>
+        <?php endif; ?>
+        <h2 class="text-uppercase mb-3"><?= $post->post_title ?> <?php if ( $subtitle ): echo '<span class="default-color"> - '. $subtitle .'</span>'; endif; ?></h2>
+        <?php if ( $tagline ): ?>
+            <h3 class="default-color"><?= $tagline ?></h3>
+        <?php endif; ?>
