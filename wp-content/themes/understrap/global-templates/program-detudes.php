007089cbf8abf509f0b3534f54f9e26fdbf5bf4a
I Kadek Pradnyana <kadek.pradnyana@gmail.com>
2019-07-31 12:25:03 +0800

    add bottom border in card title

--- wp-content/themes/understrap/global-templates/program-detudes.php
+++ wp-content/themes/understrap/global-templates/program-detudes.php
@@ -2,6 +2,8 @@
     // program data
     $programs = get_field( 'programs', 'options' );
     $fixed_card = get_field( 'fixed_Card', 'options' );
+    $fc_short_description = wp_trim_words( $fixed_card['content'] );
+    $fc_short_description = strlen( $short_description ) > 100 ? substr( $fc_short_description, 0, 100 ) .'...' : $fc_short_description;
 ?>
 <div class="container mt-5">
     <h2 class="text-uppercase text-bold text-center"><?= __('Our Programs of Study', 'cestm') ?></h2>
@@ -38,8 +40,8 @@
         <div class="col-md-4 margin-bottom-mobile">
             <div class="card kesato-card card-outline">
                 <div class="card-body card-text-only">
-                    <h5 class="card-title mt-5"><?= $fixed_card['title'] ?></h5>
-                    <p class="card-text"><?= $fixed_card['content'] ?></p>
+                    <h5 class="card-title mt-5 title-bottom-border"><?= $fixed_card['title'] ?></h5>
+                    <p class="card-text"><?= $fc_short_description ?></p>
                     <a href="<?= $fixed_card['link']['url'] ?>" class="btn btn-default btn-hover-outline d-block flex-end"><?= $fixed_card['link']['title'] ?></a>
                 </div>
             </div>
